0dd1f51e15f26ae06b27157fd9c009a3e0f0270a
Laviel <fafansin@gmail.com>
2023-02-22 12:35:29 +0300

    Spinal Tap Case - COMPLETED

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 /**
- *  Wherefore art thou
+ *  Spinal Tap Case
  * 
- * Make a function that looks through an array of objects (first argument) and returns an array of all objects that have 
- * matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the 
- * object from the collection if it is to be included in the returned array.
- * 
- * For example, if the first argument is 
- * [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], 
- * and the second argument is { last: "Capulet" }, 
- * then you must return the third object from the array (the first argument), 
- * because it contains the name and its value, that was passed on as the second argument.
+ *  Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
  */
-function whatIsInAName(collection, source) {
-    const total = Object.keys(source).length;
-    return collection.reduce((ref, item) => {
-            let counter = 0;
-            for(const key in source){
-                if(item.hasOwnProperty(key) && item[key] == source[key]){
-                    counter++;
-                }
-            }
-            if(counter == total){
-                ref.push(item);
-            }
-        
-        return ref;
-    }, [])
-
+function spinalCase(str) {
+    // return str.replace(/_/g,'-');
+    return str.replace(/_/g,'-').match(/([A-Z]+|[A-Z]?[a-z]+)(?=[A-Z]|\b)+/g).join('-').toLowerCase();
 }
-
-console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
-console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }));
+  
+console.log(spinalCase('This_Is_Spinal-Tap'));
+console.log(spinalCase('AllThe-small Things'));

19ebdc98229c723819c0cbb420dfbe0606ee7198
Laviel <fafansin@gmail.com>
2023-02-23 13:35:29 +0300

    Sum All Odd Fibonacci Numbers - COMPLETE

--- index.js
+++ index.js
@@ -10,8 +10,24 @@
  * For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
  */
 
-function sumFibs(num) {
-  return num;
+function sumFibs(number) {
+  let n1 = 0; 
+  let n2 = 1;
+  let next;
+  let sum = 0;
+
+  while(n2 <= number){
+
+    next =  n1 + n2;
+    n1 = n2;
+    n2 = next;
+    
+    if(n1 %2 > 0){
+      console.log(n1);
+      sum += n1;
+    }
+  }
+  return sum;
 }
 
-sumFibs(4);
+console.log('answer ', sumFibs(10));

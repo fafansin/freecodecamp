123d84fa51e9e59b17c2b7ca5839ec292e4b1d27
Laviel <fafansin@gmail.com>
2023-02-23 11:51:42 +0300

    Convert HTML Entities -- START

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 /**
- *  Sorted Union
+ *  Convert HTML Entities
  * 
- * Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
- * In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
- * The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
- * Check the assertion tests for examples.
+ * Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
  */
 
-function uniteUnique(...args) {
+function convertHTML(str) {
+  const ref = {
+          '&' : '&amp;', 
+          '<' : '&lt;',
+          '>' : '&gt;',
+          '"' : '&quot;'
+        }
   
-  return args.flat().reduce((final, item, idx) => {
-      if(final.indexOf(item) < 0){
-        final.push(item);
-      }
-      
-      return final;
-  }, [])
+  return str.includes('&') ? str.replace('&', ref['&']) : str;
 }
 
-console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+console.log(convertHTML("Dolce & Gabbana"));

7a73fcf7bc7c5a3cd764e14a024530897519ddbe
Laviel <fafansin@gmail.com>
2023-02-23 11:38:08 +0300

    Sorted Union -- COMPLETE

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 /**
- *  Missing letters
+ *  Sorted Union
  * 
- * Find the missing letter in the passed letter range and return it.
- * If all letters are present in the range, return undefined.
+ * Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+ * In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+ * The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+ * Check the assertion tests for examples.
  */
 
-function fearNotLetter(str) {
-  let alphabet = "abcdefghijklmnopqrstuvwxyz";
-  // console.log(alphabet[str.length-1])
-  alphabet = alphabet.substring(alphabet.indexOf(str[0]), alphabet.indexOf(str[str.length-1]) + 1);
-  for(const ltr of alphabet){
-    if(!str.includes(ltr)){
-      return ltr;
-    }
-  }
-  return undefined;
+function uniteUnique(...args) {
+  
+  return args.flat().reduce((final, item, idx) => {
+      if(final.indexOf(item) < 0){
+        final.push(item);
+      }
+      
+      return final;
+  }, [])
 }
 
-console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"));
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));

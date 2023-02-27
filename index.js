3bbf96b63c58c5819225fd415e2191c0985d7976
Laviel <fafansin@gmail.com>
2023-02-27 13:45:30 +0300

    Everything Be True - COMPLETE

--- index.js
+++ index.js
@@ -11,7 +11,8 @@
  */
 
 function truthCheck(collection, pre) {
-  return pre;
+  
+  return collection.every(item => item[pre]);
 }
 
-console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "name"));

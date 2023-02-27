ea60c49affb0fdcf7eaa3442fa86b3ea039eb589
Laviel <fafansin@gmail.com>
2023-02-27 13:38:07 +0300

    Everything Be True - Start

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 /**
- *  Binary Agents
+ *  Everything Be True
  * 
- *  Return an English translated sentence of the passed binary string.
- *  The binary string will be space separated
+ *  Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+ *  In other words, you are given an array collection of objects. 
+ *  The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+ * 
+ *  In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+ * 
+ *  Remember, you can access object properties through either dot notation or [] notation.
  */
 
-function binaryAgent(str) {
-  return str.split(" ").map(item => String.fromCharCode(parseInt(item, 2))).join('');
+function truthCheck(collection, pre) {
+  return pre;
 }
 
-console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));

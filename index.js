c3cc489749c7147683423790877cc66ef743db92
Laviel <fafansin@gmail.com>
2023-02-27 10:40:39 +0300

    Sum All Primes - COMPLETE

--- index.js
+++ index.js
@@ -9,7 +9,20 @@
  */
 
 function sumPrimes(num) {
-  return num;
+  let sum = 0;
+  for(let i = 2; i <= num; i++){
+    if(isPrime(i)){
+      sum += i
+    }
+  }
+  return sum;
+}
+
+const isPrime = num => {
+  for(let i = 2; i < num; i++) {
+      if(num % i === 0) return false;
+  }
+  return num > 1;
 }
 
 console.log(sumPrimes(10));

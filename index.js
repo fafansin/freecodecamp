8a81604744001907caf0244e5d04f84371fb915a
Laviel <fafansin@gmail.com>
2023-02-28 12:01:56 +0300

    Make a Person - COMPLETE

--- index.js
+++ index.js
@@ -13,11 +13,34 @@
 const Person = function(firstAndLast) {
   // Only change code below this line
   // Complete the method below and implement the others similarly
+  
   this.getFullName = function() {
-    return "";
-  };
-  return firstAndLast;
+    return firstAndLast;
+  }
+  this.getFirstName = function() {
+    return firstAndLast.split(" ")[0];
+  }
+  this.getLastName = function() {
+    return firstAndLast.split(" ")[1];
+  }
+  this.setFirstName = function(first){
+    const last = this.getFullName().split(" ")[1];
+    firstAndLast = `${first} ${last}`;
+  }
+  this.setLastName = function(last){
+    const first = firstAndLast.split(" ")[0];
+    firstAndLast = `${first} ${last}`;
+  }
+  this.setFullName = function(fl){
+    firstAndLast = fl;
+  }
 };
 
 const bob = new Person('Bob Ross');
+// bob.setFirstName("Baliw")
+// bob.setLastName("Baliw")
+bob.setFullName("taena ka")
 console.log(bob.getFullName());
+// console.log(bob.getFirstName());
+// console.log(bob);
+console.log(Object.keys(bob).length);

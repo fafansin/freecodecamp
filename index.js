b223b9ce2e9f1a8ff060760c8ae6d1ebc3032f89
Laviel <fafansin@gmail.com>
2023-02-28 12:03:16 +0300

    Map the Debris - START

--- index.js
+++ index.js
@@ -1,46 +1,12 @@
 /**
- *  Make a Person
+ *  Map the Debris
  * 
- *  Fill in the object constructor with the following methods below:
- *  getFirstName()
- *  getLastName()
- *  getFullName()
- *  setFirstName(first)
- *  setLastName(last)
- *  setFullName(firstAndLast)
  */
 
-const Person = function(firstAndLast) {
-  // Only change code below this line
-  // Complete the method below and implement the others similarly
-  
-  this.getFullName = function() {
-    return firstAndLast;
-  }
-  this.getFirstName = function() {
-    return firstAndLast.split(" ")[0];
-  }
-  this.getLastName = function() {
-    return firstAndLast.split(" ")[1];
-  }
-  this.setFirstName = function(first){
-    const last = this.getFullName().split(" ")[1];
-    firstAndLast = `${first} ${last}`;
-  }
-  this.setLastName = function(last){
-    const first = firstAndLast.split(" ")[0];
-    firstAndLast = `${first} ${last}`;
-  }
-  this.setFullName = function(fl){
-    firstAndLast = fl;
-  }
-};
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  return arr;
+}
 
-const bob = new Person('Bob Ross');
-// bob.setFirstName("Baliw")
-// bob.setLastName("Baliw")
-bob.setFullName("taena ka")
-console.log(bob.getFullName());
-// console.log(bob.getFirstName());
-// console.log(bob);
-console.log(Object.keys(bob).length);
+console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]));

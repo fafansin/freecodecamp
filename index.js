fe6ea469631874b26c954cfbc34f33cfe1d26772
Laviel <fafansin@gmail.com>
2023-03-02 08:57:09 +0300

    Paldimore Checker - START

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 /**
- *  Map the Debris
+ *  Palindrome Checker
+ *  Return true if the given string is a palindrome. Otherwise, return false.
+ *  A palindrome is a word or sentence that's spelled the same way both forward and backward, 
+ *  ignoring punctuation, case, and spacing.
  * 
+ *  Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) 
+ *  and turn everything into the same case (lower or upper case) in order to check for palindromes.
+ * 
+ *  We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR among others.
+ *  We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
  */
 
-function orbitalPeriod(arr) {
-  const GM = 398600.4418;
-  const earthRadius = 6367.4447;
-
-  return arr.map(item => {
-    return {name:item.name, orbitalPeriod:Math.round((2*Math.PI)*Math.sqrt((Math.pow((item.avgAlt + earthRadius),3)/GM)))};
-  });
+function palindrome(str) {
+  return true;
 }
 
-console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]));
+palindrome("eye");

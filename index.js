05e9829e4d9b4380a972331443f1979110bdb5f7
Laviel <fafansin@gmail.com>
2023-03-02 11:45:33 +0300

    Telephone Number Validator - START

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 /**
- *  Ceasars Cipher
+ *  Telephone Number Validator
  * 
- *  One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. 
- *  In a shift cipher the meanings of the letters are shifted by some set amount.
+ *  Return true if the passed string looks like a valid US phone number.
  * 
- *  A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. 
- *  Thus A ↔ N, B ↔ O and so on.
- * 
- *  Write a function which takes a ROT13 encoded string as input and returns a decoded string.
- *  All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), 
- *  but do pass them on.
  */
 
-function rot13(str) {
-  const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let ref = '';
-  for(let item of str){
-    let index = alpha.indexOf(item);
-    ref += index === -1 ? item : alpha[index + 13];
-  }
-  return ref;
+function telephoneCheck(str) {
+  return true;
 }
 
-console.log(rot13("SERR PBQR PNZC"));
-console.log(rot13("SERR CVMMN!"));
+telephoneCheck("555-555-5555");

7b17bd24775b5b0a899e9c4e1cc831b4f5062d0a
Laviel <fafansin@gmail.com>
2023-03-02 13:34:52 +0300

    Telephone Number Validator - InProgress

--- index.js
+++ index.js
@@ -6,7 +6,42 @@
  */
 
 function telephoneCheck(str) {
-  return true;
+  let process = false;
+  
+  if(/^[0-9\(]/.test(str)){
+    process = true;
+  }else{
+    return false;
+  }
+  if(/(?<=\d)\)/.test(str) || /\((?=\d)/.test(str)){
+    // if(/(?<=\d)\)/.test(str) && /\((?=\d)/.test(str)){
+    if(/\(\d{3}\)/.test(str)){
+      process = true;
+    }else{
+      return false;
+    }
+  }else{
+    process = true;
+  }
+  //
+  if(/[?]/g.test(str)){
+    return false;
+  }
+  if(process){
+    let clean = str.replace(/\D/g, '');
+    return ((clean.length == 11 && clean[0] === "1") || clean.length == 10);
+  }
+  return process;
+  // console.log(clean)
+  // console.log("testig", "-1" == "1")
+  // return ((clean.length == 11 && clean[0] === "1") || clean.length == 10);
 }
 
-telephoneCheck("555-555-5555");
+console.log(telephoneCheck("55 55-55-555-5"));
+console.log(telephoneCheck("(555)5(55?)-5555"));
+console.log(telephoneCheck("1 (555)555-5555"));
+console.log(telephoneCheck("1 (555555)-5555"));
+console.log(telephoneCheck("1 555-555-5555"));
+console.log(telephoneCheck("1 (31)555-5555"));
+console.log(telephoneCheck("1(7 576 227382"));
+console.log(telephoneCheck("(1 757) 622-7382"));

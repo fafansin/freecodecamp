7eb135a82f4db13a2e1b6cf83e3dfc921c01b8fd
Laviel <fafansin@gmail.com>
2023-03-02 11:16:05 +0300

    Ceasers Cipher - START

--- index.js
+++ index.js
@@ -1,65 +1,19 @@
 /**
- *  Roman Numeral Converter
+ *  Ceasars Cipher
  * 
- *  Convert the given number into a roman numeral
+ *  One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. 
+ *  In a shift cipher the meanings of the letters are shifted by some set amount.
+ * 
+ *  A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. 
+ *  Thus A ↔ N, B ↔ O and so on.
+ * 
+ *  Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+ *  All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), 
+ *  but do pass them on.
  */
 
-function getSymbol(divident, divisor){
-  const data = {
-    1:'I',
-    5:'V',
-    10:'X',
-    50:'L',
-    100:'C',
-    500:'D',
-    1000:'M'
-  }
-  let s = '';
-  //
-  let curr = data[divisor];
-  let half = data[divisor * 5];
-  let up = data[divisor * 10];
-  //
-  let reps = '';
-  
-  if(divident > 0){
-    if(divident == 9){
-      s = curr + up;
-    }else if(divident >= 5){
-      s = half;
-      s += repeater(curr, divident-5);
-    }else if(divident == 4){
-      s = curr + half;
-    }else{
-      s += repeater(curr, divident);
-    }
-  }
-  return s;
-  
+function rot13(str) {
+  return str;
 }
 
-function repeater(symbol, times){
-  let ref = '';
-  for(let i=1; i<= times; i++){
-    ref += symbol;
-  }
-  return ref;
-}
-function convertToRoman(num) {
-  let magic = '';
-  let divisor = 1000;
-  while(divisor >= 1){
-    let divident = Math.floor(num/divisor);
-    if(divident >= 1){
-      magic += getSymbol(divident, divisor);
-    }else{
-    }
-    num -= divident * divisor;
-    divisor /= 10;
-  }
-  return magic;
-
- }
- 
-//  console.log(convertToRoman(3946));
- console.log(convertToRoman(3999));
+console.log(rot13("SERR PBQR PNZC"));

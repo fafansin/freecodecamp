64575b997afcf3ace7da6f2437e8b8b2990576da
Laviel <fafansin@gmail.com>
2023-03-02 08:55:13 +0300

    Map the Debris - COMPLETE

--- index.js
+++ index.js
@@ -6,7 +6,10 @@
 function orbitalPeriod(arr) {
   const GM = 398600.4418;
   const earthRadius = 6367.4447;
-  return arr;
+
+  return arr.map(item => {
+    return {name:item.name, orbitalPeriod:Math.round((2*Math.PI)*Math.sqrt((Math.pow((item.avgAlt + earthRadius),3)/GM)))};
+  });
 }
 
 console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]));
